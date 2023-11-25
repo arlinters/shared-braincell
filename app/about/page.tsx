a79646cb61b82798a50adc6f233351d750f45b9c
Charles Winters III <charlie.winters17@gmail.com>
2023-11-24 17:23:38 -0800

    add cringe chat-gpt generated garbo-text

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,9 +1,36 @@
-import { title } from "@/components/primitives";
+import { subtitle, title } from "@/components/primitives";
 
 export default function AboutPage() {
 	return (
 		<div>
-			<h1 className={title()}>About</h1>
+			<h1 className={title()}>Welcome to&nbsp;</h1>
+			<h1 className={title({ color: "violet" })}>Shared Braincell&nbsp;</h1>
+			<p>
+We're not just a guild; we're a pack of gamers who are fed up with mediocrity and ready to unleash our anger on Azeroth.
+			</p>
+			<h2 className={subtitle()}>Who Are We?</h2>
+			<p>
+At <b>Shared Braincell</b>, we're a bunch of gamers from different backgrounds, and let me tell you, we're not here to hold hands and sing Kumbaya. We've got veterans who've seen it all, and we've got newcomers who better buckle up because it's going to be a rough ride.
+			</p>
+			
+			<h2 className={subtitle()}>Why Choose <b>Shared Braincell</b>?</h2>
+			<p>
+Listen up, because I'm not repeating myself. We've crushed every raid, dominated every arena, and set records that make other guilds look like a joke. When you roll with <b>Shared Braincell</b>, you're not just playing a game; you're signing up for a relentless pursuit of victory, and we don't take prisoners.
+			</p>
+
+			<h2 className={subtitle()}>What Sets Us Apart?</h2>
+				
+			<p>
+It's not all rainbows and butterflies here. Our leaders don't coddle you; they push you to your limits. We're not building connections; we're forging alliances of steel. If you can't handle the heat, then maybe <b>Shared Braincell</b> isn't for you. We're not just a guild; we're a force to be reckoned with, and we're ready to unleash our fury.
+			</p>
+
+			<p>
+So, if you think you've got what it takes to survive in our world, hit that join button and prepare for the rage-fueled journey ahead. Azeroth won't know what hit it when <b>Shared Braincell</b> comes knocking. 🔥
+			</p>
+
+
+
+
 		</div>
 	);
 }
